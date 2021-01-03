28bfd5ad9a1fe7d468fa8b3fcfa208bb73221d9a
Sirawit <sirawit.aarm@gmail.com>
2021-01-03 23:54:19 +0700

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./styles/app.scss";
+import Player from "./components/Player";
+import Song from "./components/Song";
+
+function App() {
+  return (
+    <div>
+      <Song />
+      <Player />
+    </div>
+  );
+}
+
+export default App;
